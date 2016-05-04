5f0d04a931d289feed887b812aa3199b3772ca4c
Graham Ollis <plicease@cpan.org>
2016-05-04 16:55:31 -0400

    cleanup prereqs

---
+++ README.md
@@ -0,0 +1,14 @@
+# Stacy [![Build Status](https://secure.travis-ci.org/Clustericious/Stacy.png)](http://travis-ci.org/Clustericious/Stacy)
+
+Service static listing for files
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Stacy.pm
+++ lib/Stacy.pm
@@ -2,8 +2,54 @@ package Stacy;
 
 use strict;
 use warnings;
+use base qw( Clustericious::App );
+use Clustericious::Log;
+use Path::Class::Dir;
 
 # ABSTRACT: Service static listing for files
 # VERSION
+
+sub startup
+{
+  my $self = shift;
+  $self->SUPER::startup(@_);
+  $self->secrets([rand]);
+
+  unless(defined $ENV{SIPSROOT})
+  {
+    WARN "SIPSROOT is not defined";
+    return;
+  }
+
+  my $archive = Path::Class::Dir->new($ENV{SIPSROOT})->subdir('archive');
+  foreach my $root ($archive->children)
+  {
+    next unless $root->basename =~ /^f[0-9]+$/;
+    $self->routes->get("/@{[ $root->basename ]}/*x" => sub {
+      my($c) = @_;
+
+      my $dir = $archive->subdir($c->req->url->path);
+      return $c->reply->not_found unless -d $dir;
+
+      $c->res->headers->content_type('text/plain');
+      $c->render(
+        text => join("\n",
+          map {
+            my $stat = $_->stat;
+            my $mode = $stat->[2];
+            join ' ',
+              ($_->is_dir ? 'd' : '-') . ($mode & 04 ? 'r' : '-') . ($mode & 02 ? 'w' : '-') . ($mode & 01 ? 'x' : '-'),
+              $stat->[3],
+              $stat->[7],
+              $stat->[9],
+              "$_"
+          } $dir->children) . "\n",
+        code => 200,
+      );
+
+    });
+  }
+
+}
 
 1;
